5b1ce37b6f32d8d94cf6e428d92e27514eced738
ZaurO5 <165468417+ZaurO5@users.noreply.github.com>
2024-05-30 19:18:40 +0400

    Add files via upload

---
+++ Capitalize/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public static class StringExtensions
+{
+    public static string Capitalize(this string input)
+    {
+        if (string.IsNullOrEmpty(input))
+        {
+            return input;
+        }
+        return char.ToUpper(input[0]) + input.Substring(1);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string testString = "hello";
+        Console.WriteLine(testString.Capitalize());
+    }
+}
